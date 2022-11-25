e0d90fd2017a3fd146921ce4d3e5aa9b095dcf5a
Miłosz Gilga <business@miloszgilga.pl>
2022-11-25 15:33:18 +0100

    create solution for file sending example via TCP/IP

---
+++ s21_pwss_lab3-tcp/receiver/receiver.cpp
@@ -0,0 +1,105 @@
+﻿#include "receiver.h"
+
+int main(int argc, char** argv)
+{
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		std::cerr << "Uruchomienie zakończone niepowodzeniem.\n";
+		return 1;
+	}
+
+	receiveFile();
+
+	WSACleanup();
+	system("pause");
+	return 0;
+}
+
+void receiveFile()
+{
+	struct sockaddr_in senderSa, receiverSa;
+	int receiverSaSize = sizeof(receiverSa);
+	int listenSocket, iResult, clientSocket;
+
+	if ((listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		std::cerr << "Nieudane utworzenie socketu.\n";
+		return;
+	}
+
+	senderSa.sin_family = AF_INET;
+	senderSa.sin_port = htons(PORT);
+	senderSa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if ((iResult = bind(listenSocket, (struct sockaddr*)&senderSa, sizeof(senderSa))) < 0)
+	{
+		std::cerr << "Nieudane polaczenie.\n";
+		if (closesocket(iResult) != 0 || closesocket(listenSocket) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+		}
+		return;
+	}
+
+	if ((iResult = listen(listenSocket, SOMAXCONN)) < 0) {
+		std::cerr << "Nie potrafie sluchac. Sadeg.\n";
+		if (closesocket(iResult) != 0 || closesocket(listenSocket) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+		}
+		return;
+	}
+
+	remove(FILE_NAME);
+	char buffer[FRAME_BUFF];
+
+	int singleFrameSize = 0;
+	FILE* fileHandler = fopen(FILE_NAME, "wb");
+
+	std::cout << "Odbiorca nasluchuje na plik...\n";
+	while (true)
+	{
+		if ((clientSocket = accept(listenSocket, NULL, NULL)) < 0) {
+			std::cerr << "Nie potrafie sie polaczyc. Sadeg.\n" << WSAGetLastError();
+			if (closesocket(iResult) != 0 || closesocket(listenSocket) != 0 || closesocket(clientSocket) != 0)
+			{
+				std::cerr << "Nieudane zamkniecie socketu.\n";
+			}
+			return;
+		}
+
+		while (true)
+		{
+			singleFrameSize = recv(clientSocket, buffer, FRAME_BUFF, 0);
+			if (singleFrameSize == 0)
+			{
+				std::cout << "Wyslano plik.\n";
+				fclose(fileHandler);
+				break;
+			}
+			if (singleFrameSize < 0)
+			{
+				std::cerr << "Nieudane odczytanie ramki pliku.\n";
+				if (closesocket(iResult) != 0 || closesocket(clientSocket) != 0)
+				{
+					std::cerr << "Nieudane zamkniecie socketu.\n";
+				}
+				break;
+			}
+			fwrite(buffer, sizeof(char), singleFrameSize, fileHandler);
+		}
+		fclose(fileHandler);
+
+		if (closesocket(clientSocket) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+			return;
+		}
+	}
+
+	if (closesocket(iResult) != 0 || closesocket(clientSocket) != 0)
+	{
+		std::cerr << "Nieudane zamkniecie socketu.\n";
+	}
+}
---
+++ s21_pwss_lab3-tcp/sender/sender.cpp
@@ -0,0 +1,98 @@
+﻿#include "sender.h"
+
+int main(int argc, char** argv)
+{
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		std::cerr << "Uruchomienie zakończone niepowodzeniem.\n";
+		return 1;
+	}
+
+	sendFile();
+
+	WSACleanup();
+	system("pause");
+	return 0;
+}
+
+void sendFile()
+{
+	struct sockaddr_in receiverSa;
+	int iResult;
+
+	if ((iResult = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		std::cerr << "Nieudane utworzenie socketu.\n";
+		return;
+	}
+
+	receiverSa.sin_family = AF_INET;
+	receiverSa.sin_port = htons(PORT);
+	if (inet_pton(AF_INET, RECV_INET, &(receiverSa.sin_addr)) < 0)
+	{
+		std::cerr << "Podany adres: " << RECV_INET << " nie jest prawidlowym adresem IPv4.\n";
+		if (closesocket(iResult) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+		}
+		return;
+	}
+
+	if (connect(iResult, (sockaddr*)&receiverSa, sizeof(receiverSa)) < 0) {
+		std::cerr << "Nieudane połączenie\n";
+		if (closesocket(iResult) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+		}
+		return;
+	}
+
+	FILE* fileHandler = fopen(FILE_NAME, "rb");
+	if (fileHandler == NULL)
+	{
+		std::cerr << "Nieudane otwarcie pliku " << FILE_NAME << ".\n";
+		if (closesocket(iResult) != 0)
+		{
+			std::cerr << "Nieudane zamkniecie socketu.\n";
+		}
+		return;
+	}
+
+	char buffer[FRAME_BUFF];
+
+	fseek(fileHandler, 0, SEEK_END);
+	int fileFullSize = ftell(fileHandler);
+	int frames = 0;
+
+	const clock_t begin_time = clock();
+
+	while ((frames * FRAME_BUFF) < fileFullSize)
+	{
+		fseek(fileHandler, (frames * FRAME_BUFF), SEEK_SET);
+		size_t singleFrameFileSize = fread(buffer, 1, FRAME_BUFF, fileHandler);
+
+		if (send(iResult, buffer, singleFrameFileSize, 0) < 0)
+		{
+			std::cerr << "Nieudane wysłanie danych.\n";
+			if (closesocket(iResult) != 0)
+			{
+				std::cerr << "Nieudane zamkniecie socketu.\n";
+			}
+			fclose(fileHandler);
+			return;
+		}
+		frames++;
+	}
+	
+	float elapseTime = float(clock() - begin_time) / CLOCKS_PER_SEC;
+
+	std::cout << "Wyslano " << frames << " ramek, kazda po " << FRAME_BUFF << " bajtow.\n";
+	std::cout << "Czas wysylania pliku: " << elapseTime << " sek.\n";
+	std::cout << "Calkowity rozmiar pliku: " << fileFullSize << " bajtow.\n";
+
+	if (closesocket(iResult) != 0)
+	{
+		std::cerr << "Nieudane zamkniecie socketu.\n";
+	}
+}
